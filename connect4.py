3a28a9585da8b6fd9d09e7ec9960cbd229136968
lmh00 <lmh004@protonmail.com>
2022-06-03 09:14:21 +0900

    1:43:00
    https://youtu.be/XGf2GcyHPhc?t=6181

--- connect4.py
+++ connect4.py
@@ -1,1 +1,24 @@
-test
+import numpy as np
+
+def create_board():
+    board = np.zeros((6,7))
+    return board
+
+board = create_board()
+game_over = False
+turn = 0
+
+while not game_over:
+    # Ask for Player 1 input:
+    if turn == 0:
+        selection = int(input("Player 1 Make your selection (0-6):"))
+
+    # Ask for Player 2 input:
+    else:
+        selection = int(input("Player 2 Make your selection (0-6):"))
+
+    turn += 1
+    turn = turn % 2
+
+board = create_board()
+print(board)
